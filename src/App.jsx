0fb979283a1fd70c6493d5d9e9419d75cb5898aa
zidvsd <rashidvisda@gmail.com>
2025-03-24 21:38:22 +0800

    Separated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 const App = () => {
-  return <div className="container mx-auto p-4 font-bold">App</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
 };
 
 export default App;
