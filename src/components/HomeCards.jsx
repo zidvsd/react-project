e4c4c8091569b87a9db84a1f9192d11da2003cf6
zidvsd <rashidvisda@gmail.com>
2025-03-25 15:44:34 +0800

    Added new routes

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./Card";
-
+import { Link } from "react-router-dom";
 const HomeCards = () => {
   return (
     <section className="py-4">
@@ -11,12 +11,12 @@ const HomeCards = () => {
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
-            <a
-              href="/jobs.html"
+            <Link
+              to="/jobs.html"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </Card>
 
           <Card bg="bg-indigo-500">
@@ -24,12 +24,12 @@ const HomeCards = () => {
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            <a
-              href="/add-job.html"
+            <Link
+              to="/add-job.html"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
+            </Link>
           </Card>
         </div>
       </div>
